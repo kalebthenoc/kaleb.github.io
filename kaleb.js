6ce102114a2570070eec6836eb11e8d34a12c254
kalebthenoc <132493245+kalebthenoc@users.noreply.github.com>
2023-05-29 14:04:50 -0700

    Add files via upload

---
+++ kaleb.js
@@ -0,0 +1,10 @@
+function rickRoll() {
+    let randomNumber = Math.random();
+  
+    if (randomNumber <= 0.5) {
+      window.open('https://www.youtube.com/watch?v=f9qgAOi9Q9E');
+    } else {
+      window.open('kaleb.html');
+    }
+  }
+  
---
+++ loginScript.js
@@ -0,0 +1,6 @@
+function signup() {
+    var formUsername = prompt('Enter Username:');
+    var formPassword = prompt('Makeup a password:');
+    alert("Welcome, " + formUsername + ".");
+    window.open('index.html');
+    }
